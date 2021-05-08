dccae718797139ed3cbef30b8a172683284ddcba
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-08 12:03:43 +0200

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Ndayisenga Jean Claude on 08/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
